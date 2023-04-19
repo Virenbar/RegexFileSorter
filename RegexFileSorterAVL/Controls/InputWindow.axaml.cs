70e9cacab1f15c4a06ea9991a4d9ad1934e11a08
Virenbar <rib.artem@gmail.com>
2023-04-19 18:26:48 +0500

    WIP

---
+++ RegexFileSorterAVL/Controls/InputWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RegexFileSorterAVL.Controls
+{
+    public partial class InputWindow : UserControl
+    {
+        public InputWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
